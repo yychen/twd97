47a96b05983576f4ee96b746aad29c2146fb96b6
Tom Chen <ctchen@gmail.com>
2014-10-22 11:01:31 +0800

    Change version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='twd97',
-      version='0.1.0',
+      version='0.1.1',
       description='Python converter between TWD97 and WGS84',
       license='MIT',
       author='Tom Chiung-ting Chen',
